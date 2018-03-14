0ed52d617ebce5eaad957263003c2a61175743d7
Nick Denry <nick.denry@gmail.com>
2018-03-15 01:52:54 +0300

    Add extension assets

---
+++ src/assets/RoundSwitchAsset.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace nickdenry\grid\toggle\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Round switch asset bundle.
+ */
+class RoundSwitchAsset extends AssetBundle
+{
+    public $sourcePath = '@nickdenry/grid/toggle/web';
+    public $css = [
+        'css/round-switch.css',
+    ];
+    public $js = [
+        'js/round-switch.js',
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
